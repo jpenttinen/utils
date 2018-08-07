fc1fb204d17afb485e14c618cdf96335ff7c070a
jpenttinen <37104899+jpenttinen@users.noreply.github.com>
2018-08-07 19:36:01 +0300

    WIP

--- make_report_files.py
+++ make_report_files.py
@@ -11,30 +11,38 @@ from decimal import Decimal
 
 def check_arg(args=None):
     ''' This function reads command line options.'''
-    parser = argparse.ArgumentParser(description='Script which generates CSV/XLSX and PNG files from JSON data')
-    parser.add_argument('-e', '--excel', action="store_true",
-                        help='Generate Excel XLSX file', default=None,)
+    parser = argparse.ArgumentParser(
+        description='Script which create CSV/XLSX and PNG files from JSON data')
+    parser.add_argument(
+        '-e', '--excel', action="store_true",
+        help='Generate Excel XLSX file', default=None)
     parser.add_argument('-i', '--input_file', required=True,
                         help='Input JSON file name, -i rts.json')
     parser.add_argument('-d', '--results_dir', default='/tmp',
                         help='Results directory, -d /tmp)')
     parser.add_argument('-c', '--csv', default=None, action="store_true",
                         help='Generate CSV file')
-    parser.add_argument('-p', '--png', default=None, action="store_true",
-                        help='Generate PNG file')
+    parser.add_argument('-ap', '--app_png', default=None, action="store_true",
+                        help='Generate Application Simulator PNG file')
+    parser.add_argument('-rp', '--rr_png', default=None, action="store_true",
+                        help='Generate Routing Robot PNG file')
     parser.add_argument('--html', default=None, action="store_true",
                         help='Generate HTML file')
     arguments = parser.parse_args(args)
     return (arguments.excel, arguments.input_file,
             arguments.results_dir, arguments.csv,
-            arguments.png, arguments.html)
+            arguments.app_png, arguments.rr_png, arguments.html)
 
 
 def main():
-    print(input_file)
-    file_name = input_file.split('.')[1]
-    file_name = file_name.strip('\\')
-    print(file_name)
+    # print(input_file)
+    if os.name == 'nt':
+        file_name = input_file.split('.')[1]
+        file_name = file_name.strip('\\')
+        # print(file_name)
+    else:
+        file_name = input_file.split('.')[0]
+
     with open(input_file) as json_file:
         data = json.load(json_file, parse_int=Decimal, parse_float=Decimal)
     df = pd.DataFrame(data)
@@ -42,15 +50,23 @@ def main():
         df.to_csv(file_name + '.csv', sep=',', index=False)
     if html:
         df.to_html(file_name + '.html')
-    if png:
+    if app_png or rr_png:
         ax = plt.gca()
         df = df.astype(float)
-        df.plot(kind='line', x='Timestamp', y='appTxFrameDataRate', ax=ax,
-                markevery=10, marker='o', markerfacecolor='black', grid=True)
-        df.plot(kind='line', x='Timestamp', y='appTxFrameDataRate', color='red', ax=ax,
-                markevery=10, marker='o', markerfacecolor='black', grid=True)
-        df.plot(kind='line', x='Timestamp', y='appFlowRate', color='brown', ax=ax,
-                markevery=10, marker='o', markerfacecolor='black', grid=True)
+        if app_png:
+            df.plot(kind='line', x='Timestamp', y='appTxFrameDataRate', ax=ax,
+                    markevery=10, marker='o', markerfacecolor='black', grid=True)
+            df.plot(kind='line', x='Timestamp', y='appTxFrameDataRate', color='red', ax=ax,
+                    markevery=10, marker='o', markerfacecolor='black', grid=True)
+            df.plot(kind='line', x='Timestamp', y='appFlowRate', color='brown', ax=ax,
+                    markevery=10, marker='o', markerfacecolor='black', grid=True)
+        elif rr_png:
+            df.plot(kind='line', x='Timestamp', y='appTxFrameDataRate', ax=ax,
+                    markevery=10, marker='o', markerfacecolor='black', grid=True)
+            df.plot(kind='line', x='Timestamp', y='appTxFrameDataRate', color='red', ax=ax,
+                    markevery=10, marker='o', markerfacecolor='black', grid=True)
+            df.plot(kind='line', x='Timestamp', y='appFlowRate', color='brown', ax=ax,
+                    markevery=10, marker='o', markerfacecolor='black', grid=True)
         # plt.show()
         plt.savefig(file_name + '.png')
     if excel:
@@ -59,6 +75,6 @@ def main():
         writer.save()
 
 if __name__ == '__main__':
-    excel, input_file, results_dir, csv, png, html = check_arg(sys.argv[1:])
+    excel, input_file, results_dir, csv, app_png, rr_png, html = check_arg(sys.argv[1:])
     os.makedirs(results_dir, exist_ok=True)
     main()
